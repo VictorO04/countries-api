44d19738cbbade6ad013e1bc0fc119ad96df4ed1
Victor <victorfoliveira0407@gmail.com>
2026-02-27 14:53:53 -0300

    feat: first commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
+const serverPort = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('API is online');
+});
+
+app.use((req, res) => {
+    res.status(404).json({
+        error: 'Route not found'
+    });
+});
+
+app.listen(serverPort, () => {
+    console.log(`- Server online at: http://localhost:${serverPort}`);
+});
