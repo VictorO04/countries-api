37cba82b600a4348b66e65756f483e8ba5f6d4c7
Victor Oliveira <victorfoliveira0407@gmail.com>
2026-02-28 17:46:11 -0300

    feat: add seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,10 +3,34 @@ import prisma from '../src/utils/prismaClient.js';
 const main = async () => {
     console.log('🌱 Starting seed...');
 
-    await prisma.flag.createMany({
+    await prisma.country.createMany({
         data: [
-
-        ]
+            {
+                name: "Brazil",
+                capital: "Brasilia",
+                continent: "South America",
+                flagUrl: "https://i.imgur.com/AUh9PAH.png"
+            },
+            {
+                name: "Argentina",
+                capital: "Buenos Aires",
+                continent: "South America",
+                flagUrl: "https://i.imgur.com/3EXm3HS.gif"
+            },
+            {
+                name: "United States",
+                capital: "Washington, D.C.",
+                continent: "North America",
+                flagUrl: "https://i.imgur.com/tFnsakL.png"
+            },
+            {
+                name: "Canada",
+                capital: "Ottawa",
+                continent: "North America",
+                flagUrl: "https://i.imgur.com/QUhvv5e.gif"
+            },
+        ],
+        skipDuplicates: true,
     });
 
     console.log('✅ Seed finished');
