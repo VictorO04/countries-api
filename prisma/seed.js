c3597aa1e0b8eaa35b5550bb7dcc62ea719ea5dd
Victor <victorcriri313@gmail.com>
2026-03-04 19:55:03 -0300

    feat: add enums

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,30 +5,7 @@ const main = async () => {
 
     await prisma.country.createMany({
         data: [
-            {
-                name: "Brazil",
-                capital: "Brasilia",
-                continent: "South America",
-                flagUrl: "https://i.imgur.com/AUh9PAH.png"
-            },
-            {
-                name: "Argentina",
-                capital: "Buenos Aires",
-                continent: "South America",
-                flagUrl: "https://i.imgur.com/3EXm3HS.gif"
-            },
-            {
-                name: "United States",
-                capital: "Washington, D.C.",
-                continent: "North America",
-                flagUrl: "https://i.imgur.com/tFnsakL.png"
-            },
-            {
-                name: "Canada",
-                capital: "Ottawa",
-                continent: "North America",
-                flagUrl: "https://i.imgur.com/QUhvv5e.gif"
-            },
+            
         ],
         skipDuplicates: true,
     });
