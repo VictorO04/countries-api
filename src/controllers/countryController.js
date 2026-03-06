952d3c59fa54df9f6d2c4bfb8ab8c3e51ba6313d
Victor <victorcriri313@gmail.com>
2026-03-05 21:17:00 -0300

    feat: add get country by id

--- src/controllers/countryController.js
+++ src/controllers/countryController.js
@@ -4,20 +4,51 @@ const serverErrorMessage = (res, error) => {
     console.error(error);
 
     res.status(500).json({
-        message: "Internal server error",
+        message: 'Internal server error',
     });
 }
+
+const validId = (id) => !Number.isNaN(id) && Number.isInteger(id) && id > 0;
 
 export const getCountries = async (req, res) => {
     try {
         const countries = await CountryModel.findAllCountries(req.query);
 
         res.status(200).json({
-            total: countries.length,
+            success: true,
             message: countries.length === 0
-                ? "No countries found"
-                : "Countries retrieved",
+                ? 'No countries found'
+                : 'Countries retrieved',
             data: countries,
+        });
+    } catch (error) {
+        serverErrorMessage(res, error);
+    }
+}
+
+export const getCountryById = async (req ,res) => {
+    try {
+        const id = Number(req.params.id);
+
+        if (!validId(id)) {
+            return res.status(400).json({
+                success: false,
+                message: 'Invalid id parameter'
+            });
+        }
+
+        const country = await CountryModel.findCountryById(id);
+        if (!country) {
+            return res.status(404).json({
+                success: false,
+                message: `Country with id ${id} not found`
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: `Country with id ${id} found`,
+            data: country
         });
     } catch (error) {
         serverErrorMessage(res, error);
--- src/routes/countryRoutes.js
+++ src/routes/countryRoutes.js
@@ -4,5 +4,6 @@ import * as countryController from '../controllers/countryController.js';
 const router = Router();
 
 router.get('/countries', countryController.getCountries);
+router.get('/countries/:id', countryController.getCountryById);
 
 export default router;
